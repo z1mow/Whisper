45cd41c703047bebf615ca72cbf57975565ad86a
ŞakirYılmaz Öğüt <sakirogut@Sakirs-MacBook-Pro-16.local>
2024-12-09 14:53:07 +0300

    Initial commit

---
+++ Whisper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whisper
+//
+//  Created by ŞakirYılmaz Öğüt on 9.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
