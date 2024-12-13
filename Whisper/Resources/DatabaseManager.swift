3afd24abf38eabfed63e04a76be0eeda6adf1f16
z1mow <zimowcs@gmail.com>
2024-12-13 11:18:27 +0300

    Fix register errors

--- Whisper/Resources/DatabaseManager.swift
+++ Whisper/Resources/DatabaseManager.swift
@@ -22,6 +22,10 @@ extension DatabaseManager {
     
     public func userExists(with email: String,
                            completion: @escaping ((Bool) -> Void)) {
+        
+        var safeEmail = email.replacingOccurrences(of: ". ", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        
         database.child(email).observeSingleEvent(of: .value, with: {snapshot in
             guard snapshot.value as? String != nil else {
                 completion(false)
@@ -34,7 +38,7 @@ extension DatabaseManager {
     
     /// Inserts new user to database
     public func insertUser(with user: ChatAppUser) {
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName,
         ])
@@ -45,5 +49,11 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let emailAddress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ". ", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
 //    let profilePictureUrl: String
 }
